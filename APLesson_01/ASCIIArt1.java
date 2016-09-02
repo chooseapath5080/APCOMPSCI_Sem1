a04cce307fe389e005130cf6a8f3ee9a35dd1652
chooseapath5080 <trueblue5080@gmail.com>
2016-09-02 13:37:42 -0700

    Added Files

---
+++ APLesson_01/ASCIIArt1.java
@@ -0,0 +1,9 @@
+public class ASCIIArt1
+{
+	public static void main (String[]args)
+	{
+		System.out.println("  (\\__/) ");
+		System.out.println("  (='.'=)");
+		System.out.println("  (\")_(\")   ");
+	}
+}
---
+++ APLesson_01/ASCIIArt2.java
@@ -0,0 +1,10 @@
+public class ASCIIArt2
+{
+	public static void main (String[]args)
+	{
+		System.out.println(" ____");
+		System.out.println("{o , o}");
+		System.out.println(" |)__ )");
+		System.out.println("  \"  \"");
+	}
+}
