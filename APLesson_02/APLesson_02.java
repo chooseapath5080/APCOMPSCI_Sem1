af9718803bb7601dd52ebf7f7d7cde4753714774
chooseapath5080 <trueblue5080@gmail.com>
2016-09-07 13:53:42 -0700

    Added Files

---
+++ APLesson_02/APLesson_02.java
@@ -0,0 +1,8 @@
+public class APLesson_02
+{
+	public static void main(String[]args)
+	{
+		String name = "BeautifulUnicorn";
+		System.out.println(name);
+	}
+}
---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,25 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 10;
+		int b = 20;
+		int product = a * b;
+		
+		System.out.println("10 " + "multiplied by " + "20 " + "is " + product);
+		System.out.println("       ");
+		
+		String name = "Amy Jeon\n";
+		String address = "13510 Blue Lace Trail\n";
+		String city = "San Diego\n";
+		String zip = "92130\n";
+		System.out.println(name + address + city + zip);
+		
+		int l = 3;
+		int w = 4;
+		int h = 5;
+		int SA = 2*w*l+2*l*h+2*h*w;
+		
+		System.out.println("The surface area of your rectangle is "+ SA);
+	}
+}
