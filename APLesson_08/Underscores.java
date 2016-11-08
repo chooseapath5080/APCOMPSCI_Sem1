58a980891409ad6134b5e1fcb2c7bc300f3fe3f8
chooseapath5080 <trueblue5080@gmail.com>
2016-11-08 14:20:09 -0800

    Lesson 08

--- APLesson_08/Underscores.java
+++ APLesson_08/Underscores.java
@@ -1,2 +1,20 @@
 import java.util.Scanner;
 public class Underscores
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter your sentence");
+		System.out.println(replace(kb.nextLine()));
+	}
+	
+	public static String replace(String sentence)
+	{
+		if(sentence.indexOf(" ") >= 0)
+		{
+			return replace(sentence.substring(0,sentence.indexOf(" ")) + "_" + sentence.substring(sentence.indexOf(" ")+1, sentence.length()));
+		}
+		else
+			return sentence;
+	}
+}
