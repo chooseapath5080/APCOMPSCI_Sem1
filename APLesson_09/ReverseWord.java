988fbde40a02bb0dcf1eadc948559581c661dcef
chooseapath5080 <trueblue5080@gmail.com>
2016-11-15 14:18:02 -0800

    Lesson 9

---
+++ APLesson_09/ReverseWord.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ReverseWord
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter your 5 words: ");
+		String [] words = new String[5];
+		
+		for(int i = 0; i < words.length; i++)
+		{
+			words[i] = kb.next();
+		}
+		
+		System.out.println("In order...");
+		
+		for(String word : words)
+			System.out.println(word);
+		System.out.println();
+		
+		System.out.println("Reversed...");
+		
+		reverse(words);
+	}
+	
+	public static void reverse(String [] words)
+	{
+		for(int i = words.length - 1; i >= 0; i--)
+			System.out.println(words[i]);
+	}
+}
