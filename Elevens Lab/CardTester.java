8fee27f7303e34178f597c6c6bbc8be72336d7e6
chooseapath5080 <trueblue5080@gmail.com>
2017-05-05 13:18:42 -0700

    Elevens lab Activity 1

--- Elevens Lab/CardTester.java
+++ Elevens Lab/CardTester.java
@@ -7,7 +7,15 @@ public class CardTester {
 	 * The main method in this class checks the Card operations for consistency.
 	 *	@param args is not used.
 	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public static void main(String[] args) 
+	{
+		Card one = new Card("9", "Clubs", 9);
+		Card two = new Card("8", "Diamonds", 8);
+		Card three = new Card("2", "Spades", 2);
+  
+		System.out.println("One matches two? " + one.matches(two));
+		System.out.println("One matches three? " + one.matches(three));    
+		System.out.println("Suit of Card One: " + one.suit());
+		System.out.println("Card one: " + one.toString());
 	}
 }
